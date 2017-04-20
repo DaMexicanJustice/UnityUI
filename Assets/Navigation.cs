9e9c6c0aa1f9d3db51f44509af23806938e4c70a
Patrick Johansen <Patrick Johansen>
2017-04-20 10:28:02 +0200

    Added final result of UI tutorial

---
+++ Assets/Navigation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Navigation : MonoBehaviour {
+
+	public Slider ageSlider;
+	public Text ageText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ageText.text = "" + ageSlider.value;
+	}
+
+	public void ExitApp() {
+		Application.Quit ();
+	}
+
+}
